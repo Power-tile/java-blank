09216416f2cfc133e06133d7bedc15376f3962f7
Power_tile <hejizheng2004@163.com>
2020-05-23 20:36:15 +0800

    add spring dependency to project

--- src/main/java/cn/moonshotacademy/scp/vendingmachine/App.java
+++ src/main/java/cn/moonshotacademy/scp/vendingmachine/App.java
@@ -1,7 +1,12 @@
 package cn.moonshotacademy.scp.vendingmachine;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+        TestClass testClass = (TestClass)ac.getBean("testClass");
+        testClass.printMessage();
     }
 }
